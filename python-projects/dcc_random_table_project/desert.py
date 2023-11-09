2ba74f3da3b2228713ed3cbada9e7efd928af478
Zeno Enderle <zenoenderle@yahoo.com>
2023-11-09 13:31:20 -0600

    modified:   python-projects/dcc_random_table_project/__pycache__/desert.cpython-311.pyc 	modified:   python-projects/dcc_random_table_project/__pycache__/events.cpython-311.pyc 	modified:   python-projects/dcc_random_table_project/__pycache__/header.cpython-311.pyc 	modified:   python-projects/dcc_random_table_project/__pycache__/mountain.cpython-311.pyc 	modified:   python-projects/dcc_random_table_project/__pycache__/ocean.cpython-311.pyc 	modified:   python-projects/dcc_random_table_project/__pycache__/travel_random.cpython-311.pyc 	modified:   python-projects/dcc_random_table_project/desert.py

--- python-projects/dcc_random_table_project/desert.py
+++ python-projects/dcc_random_table_project/desert.py
@@ -1,8 +1,27 @@
+
+
 DESERT_TRAVEL_EVENTS = {
-    1: "Desert 24-25 Human Druid of 7th level\n in a camouflaged tree house (AC 8; MV 12; HP 42; #AT 1; DMG 1d8+3 [scimitar +2] or 1d6+1 [spear]; STR 16, INT 13, WIS 16, DEX 9, CON 15, CHA 15; SD identify plants, animals, and pure water; pass through overgrown areas without leaving trail at normal MV; immunity to charm spells cat by woodland creatures; assume animal form 3/day; +2 save against fire and lightning; AL N, SZ M; XP 1,415; Spells: [1st-level: Animal Friendship, Detect Magic, Purify Water, Speak With Animals; 2nd-level: Charm Person or Mammal, Create Water, Cure Light Wounds, Feign Death; 3rd-level: Call Lightning, Plant Growth, Summon Insects; 4th-level: Cure Serious Wounds]; Equipment: leather armor, scimitar +2, spear, potion of green dragon control, dagger, mistletoe, 2d10+2 gp, 3d10+3 sp, 4d10+4 cp)",
-    2: {"name": "Patrols will be of a racial composition appropriate to the area. Patrols will always be mounted unless there are extenuating circumstances such as terrain or a nation which shuns mounts. Leaders will ride warhorses. Patrols will be commanded by a fighter (or ranger, where applicable) of from 6th to 8th level, with a lieutenant of from 4th to 5th level, and a serjeant of 2nd or 3rd level. There will be from 3 to 4 1st level men and from 13 to 24 soldiers (men-at-arms) forming the main body. Armor and arms will be plate mail, shield, lance, flail, and long sword for all fighters of 1st level and above. Soldiers will typically have chain or scale mail, shield, bow or light crossbow,", "table_size": 2},
-    3: {"name": "Mountain that is tal", "table_size": 3},
-    4: {"name": "Random dude", "table_size": 4},
+    1: "\n\n\n\n\n\n--------------------\nencounter\n--------------------\nCR: 2\n\n17 Scorpions\nAC: 1 HP: 1 ATK: +2 DMG: +1 \n\nLink: https://5e.tools/bestiary.html#scorpion_mm \n\n\n\n\n\n\n\n\n",
+    2: "4 vultures",
+    3: "1 abandoned mule",
+    4: "6 commoners with 2d4 camels bound for a distant city",
+    5: "6 flying snakes",
+    6: "6 hyenas or 2d6 jackals",
+    7: "6 guards escorting a noble to the edge of the desert, all of them astride camels",
+    8: "6 cats",
+    9: "1 pseudodragon",
+    10: "4 Poisonous Snakes",
+    11: "4 stirges",
+    12: "4 Wolf Spiders",
+    13: "1 Scout for a nameless organization",
+    14: "4 giant poisonous snakes",
+    15: "Single file boot tracks in the sand",
+    16: "4 Kobolds",
+    17: "1 Jackalwere",
+    18: "6 tribale warriors",
+    19: "6 Giant Lizards",
+    20: "1 Swarm of Inscects",
+    21: ""
 }
 
-SIZE_OF_DESERT = 4
+SIZE_OF_DESERT = 5
