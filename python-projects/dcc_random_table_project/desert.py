266159ff60f49c44e1c96c845638bc3b565049bb
Zeno Enderle <zenoenderle@yahoo.com>
2023-11-16 15:39:43 -0600

    modified:   python-projects/dcc_random_table_project/desert.py

--- python-projects/dcc_random_table_project/desert.py
+++ python-projects/dcc_random_table_project/desert.py
@@ -1,7 +1,7 @@
 
 
 DESERT_TRAVEL_EVENTS = {
-    1: "\n\n\n\n\n\n--------------------\nencounter\n--------------------\nCR: 2\n\n17 Scorpions\nAC: 1 HP: 1 ATK: +2 DMG: +1 \n\nLink: https://5e.tools/bestiary.html#scorpion_mm \n\n\n\n\n\n\n\n\n",
+    1: "\n\n\n\n\n\n--------------------\nencounter\n--------------------\nCR: 2\nAC: 1 HP: 1 ATK: +2 DMG: +1 \n17 Scorpions\n\nLink: https://5e.tools/bestiary.html#scorpion_mm \n\n\n\n\n\n\n\n\n",
     2: "4 vultures",
     3: "1 abandoned mule",
     4: "6 commoners with 2d4 camels bound for a distant city",
@@ -21,7 +21,18 @@ DESERT_TRAVEL_EVENTS = {
     18: "6 tribale warriors",
     19: "6 Giant Lizards",
     20: "1 Swarm of Inscects",
-    21: ""
+    21: "An oasis surrounded by palm trees and containing the remnants of an old camp",
+    22: "6 bandits",
+    23: "4 constrictor snakes",
+    24: "4 winged Kobolds",
+    25: "1 dust mephit",
+    26: "3 giant toads",
+    27: "4 giant spiders",
+    28: "1 druid, Druid named Academi he is a elf with a quest for the players to retrieve some stolen weapons, Offers 900GP (DC 18 Persuasion check for more money)",
+    29: "4 Hobgoblins",
+    30: "1 Wight",
+    31: "1 Ogre and 1 el-goblino",
+    32: "A single Brass Lamp on the ground, there is a faint oil trail (DC 20 Investigation check to find where the trail leads) the trail leads goes into a cave.",
 }
 
 SIZE_OF_DESERT = 21
